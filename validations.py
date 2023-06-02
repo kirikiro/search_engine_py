c329c688ba9655e63991558bbb4494a22b436488
kirikiro <126089768+kirikiro@users.noreply.github.com>
2023-06-02 08:20:35 +0300

    Update validations.py

--- validations.py
+++ validations.py
@@ -1,3 +1,7 @@
 
-def validation(value):
-    return True
+
+def validation(request):
+    if 'text' in request and 'created_date' in request and 'rubrics' in request:
+        if isinstance(request['text'], str) and isinstance(request['created_date'], str) and isinstance(request['rubrics'], list):
+            return True
+    return False
